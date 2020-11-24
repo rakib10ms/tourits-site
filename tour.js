04b383634fbf0979f0b4dcb8c8bcaf93bda994b3
Rakib <your_email@example.com>
2020-11-24 22:49:52 +0600

    initial commit added

---
+++ tour.js
@@ -0,0 +1,24 @@
+
+const showCase=document.querySelector('.form');
+showCase.addEventListener('submit', showMessage)
+
+
+function showMessage(){
+         alert("Your message was sent")
+      
+}
+
+/*
+
+const arrow=document.querySelector('.arrow');
+
+window.addEventListener('scroll',(e) => {
+    const scrolled=window.scrollY;
+    if(scrolled>=600){
+        arrow.style.classList.add('activee')    }
+    else{
+        arrow.style.classList.remove('activee')
+
+    }
+});
+*/
